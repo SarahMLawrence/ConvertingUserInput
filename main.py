f0c97e303cca989679de3af171c0b0e22bbdbc99
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-10-14 23:39:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+inp = input('Europe floor? ')
+usf = int(inp) + 1
+print('US floor: ', usf)
+
+
+# if we want to read a number from the user, we must convert it from a string to a number using a type conversion function
